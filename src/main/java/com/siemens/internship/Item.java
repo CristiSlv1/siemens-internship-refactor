2e1a7e6f19600eb8ceb720592064a196aaa56ee7
CristiSlv1 <strugaricristian9@gmail.com>
2025-05-10 23:42:19 +0300

    Update Item.java
    implementing email validation from jakarta; adding getters and setters

--- src/main/java/com/siemens/internship/Item.java
+++ src/main/java/com/siemens/internship/Item.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,10 +21,55 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
     private String description;
+
     private String status;
 
-    // Add email regex validation
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email cannot be empty")
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
